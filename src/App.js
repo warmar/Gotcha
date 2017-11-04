64614b5a37675e749b1be56e721d2fa5c39da2a7
Peter Marangos <lifelessmango@gmail.com>
2017-11-04 10:56:03 -0400

    Add basic login functionality

--- src/App.js
+++ src/App.js
@@ -14,15 +14,64 @@ var config = {
 firebase.initializeApp(config);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: null
+    };
+
+    this.login = this.login.bind(this);
+    this.signOut = this.signOut.bind(this);
+    this.gotOut = this.gotOut.bind(this);
+  }
+
+  login() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then((result) => {
+      // Handle Login
+
+      //var token = result.credential.accessToken;
+      
+      var user = result.user;
+
+      this.setState({
+        email: user.email
+      });
+    }).catch((error) => {
+      // Handle Login Errors
+      console.log(error)
+    });
+  }
+
+  gotOut() {
+
+  }
+
+  signOut() {
+    firebase.auth().signOut().then(() => {
+      // Sign-out successful.
+      console.log('Successfully signed out')
+      this.setState({
+        email: null
+      });
+    }).catch((error) => {
+      // An error happened.
+      console.log('Error while signing out')
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Gotcha</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <button onClick={this.login}>Login</button>
+          <button onClick={this.signOut}>Sign Out</button>
+          {this.state.email}
         </p>
       </div>
     );
