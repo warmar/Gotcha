3dd5640bf03fc28f22c4e50436782e3f0361f963
Peter Marangos <lifelessmango@gmail.com>
2017-11-04 14:17:05 -0400

    App.js Remove console.log

--- src/App.js
+++ src/App.js
@@ -67,9 +67,8 @@ class App extends Component {
   }
 
   gotOut() {
-    var database = firebase.database()
-    console.log(this.state.user.uid)
-    return database.ref(`/users/${this.state.user.uid}/gotout`).set(true)
+    var database = firebase.database();
+    return database.ref(`/users/${this.state.user.uid}/gotout`).set(true);
   }
 
   registerDatabaseListeners() {
